005f2d7b4d5c757353b60ee38b8f5fd02c75a1e3
Lívia <livia.matos2721@gmail.com>
2025-05-06 15:13:27 -0300

    Atualizando atividade 8

--- atvOito.ts
+++ atvOito.ts
@@ -1,7 +1,7 @@
 /* 10 pontos
 8 - Leia a idade e o sexo de 50 pessoas e mostre a média de idade para
 cada sexo.
-Nome aluno: Lívia Borges Matos  ARRUMAR CÓDIGO!!
+Nome aluno: Lívia Borges Matos  
 */        
 
 let teclado = require('prompt-sync')();
@@ -9,25 +9,29 @@ let idade: number = 0;
 let sexo: string = '';
 let contFem: number = 0;
 let contMasc: number = 0;
-let mediaMasc: number = 0;
-let mediaFem: number = 0;
 let contPessoa: number = 0;
-let somaIdade: number = 0;
+let somaIdadeF: number = 0;
+let somaIdadeM: number = 0;
 
-for(contPessoa = 1; contPessoa <=50; contPessoa++){
+for(contPessoa = 1; contPessoa <=5; contPessoa++){
     idade = parseInt(teclado('Digite sua idade:'));
     sexo = teclado('Digite seu sexo, insira f/m, para feminino ou masculino:');
-    somaIdade += idade;
-}
-if( sexo == 'f'){
-    contFem++
-}
-else{
-    contMasc++
+    
+
+    if( sexo == 'f'){
+        contFem++
+        somaIdadeF += idade;
+    }
+    else{
+        contMasc++;
+        somaIdadeM += idade;
+    }
 }
 
-mediaFem = somaIdade/contFem
-mediaMasc = somaIdade/contMasc
+
+let mediaFem = somaIdadeF/contFem
+let mediaMasc = somaIdadeM/contMasc
+
 console.log(`Há ${contFem} pessoas do sexo feminino, e há  ${contMasc} do sexo Masculino.
     Então, a média de pessoas do sexo feminino é de: ${mediaFem}, e a média de pessoas do 
     sexo Masculino é de : ${mediaMasc}`);
